7f10bf1af68e1d544404383af5a4c2fddfe63dea
zdw <252675163@qq.com>
2020-08-09 20:07:15 +0800

    feat: init-data-without-props-or-computer

--- tests/lib/rules/init-data-without-props-or-computer.js
+++ tests/lib/rules/init-data-without-props-or-computer.js
@@ -3,27 +3,27 @@
  * @author OBKoro1
  */
 "use strict";
-var rule = require("../../../lib/rules/no-spui"), // 引入rule
+var rule = require("../../../lib/rules/init-data-without-props-or-computer"), // 引入rule
   RuleTester = require("eslint").RuleTester;
-
 var ruleTester = new RuleTester({
   parser: require.resolve("vue-eslint-parser"),
-  parserOptions: {
-    ecmaVersion: 7, // 默认支持语法为es5
-    parser: '@typescript-eslint/parser'
-  },
+  "parserOptions": {
+    ecmaVersion: 2020, sourceType: 'module',
+    "parser": "@typescript-eslint/parser"
+  }
 });
 // 运行测试用例
-ruleTester.run("no-spui", rule, {
+ruleTester.run("init-data-without-props-or-computer", rule, {
   // 正确的测试用例
   valid: [
-    
+
   ],
   // 错误的测试用例
   invalid: [
     {
+      filename: "a.vue",
       code: `
-      <script lang='ts'>
+      <script lang="ts">
       export default class InfoDataComponent extends Vue{
         @Prop({ default: 0 })
         pageType!: number
